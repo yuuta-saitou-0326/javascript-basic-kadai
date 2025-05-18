c8fd623f6780c6928c7df3b3aeb705ee17e11426
yuuta-saitou-0326 <taabko.kool12@gmail.com>
2025-05-18 19:48:17 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,13 +1,13 @@
-let num = 1;
+let num = 155;
 
-if (num % 3 == 0) {
-  console.log('3の倍数です')
+if (num % 3 == 0 && num % 5 == 0) {
+  console.log('3と5の倍数です')
 }
 else if (num % 5 == 0) {
   console.log('5の倍数です')
 }
-else if (num % 3 == 0 && num % 5 == 0) {
-  console.log('3と5の倍数です')
+else if (num % 3 == 0) {
+  console.log('3の倍数です')
 }
 else {
   console.log(num)
